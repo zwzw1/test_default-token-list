a292cc00473a4f30e177045eeefddc10c7f13933
cj <745960380@qq.com>
2022-08-12 17:27:16 +0800

    init

--- src/buildList.js
+++ src/buildList.js
@@ -1,32 +1,31 @@
-const { version } = require('../package.json');
-const mainnet = require('./tokens/mainnet.json');
-const ropsten = require('./tokens/ropsten.json');
-const rinkeby = require('./tokens/rinkeby.json');
-const goerli = require('./tokens/goerli.json');
-const kovan = require('./tokens/kovan.json');
+const { version } = require("../package.json");
+const mainnet = require("./tokens/mainnet.json");
+const ropsten = require("./tokens/ropsten.json");
+const rinkeby = require("./tokens/rinkeby.json");
+const goerli = require("./tokens/goerli.json");
+const kovan = require("./tokens/kovan.json");
+const aeonTest = require("./tokens/aeonTest.json");
 
 module.exports = function buildList() {
-  const parsed = version.split('.');
+  const parsed = version.split(".");
   return {
-    'name': 'Uniswap Default List',
-    'timestamp': (new Date().toISOString()),
-    'version': {
-      'major': +parsed[ 0 ],
-      'minor': +parsed[ 1 ],
-      'patch': +parsed[ 2 ]
+    name: "Uniswap Default List",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
     },
-    'tags': {},
-    'logoURI': 'ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir',
-    'keywords': [
-      'uniswap',
-      'default'
-    ],
+    tags: {},
+    logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
+    keywords: ["uniswap", "default"],
     tokens: [
       ...mainnet,
       ...ropsten,
       ...goerli,
       ...kovan,
-      ...rinkeby
+      ...rinkeby,
+      ...aeonTest,
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
@@ -34,6 +33,6 @@ module.exports = function buildList() {
           return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
-      })
+      }),
   };
 };
