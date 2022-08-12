1860d837e40460c6274758b2bb1aca3f2090756c
cj <745960380@qq.com>
2022-08-12 17:15:54 +0800

    init

--- src/buildList.js
+++ src/buildList.js
@@ -1,37 +1,32 @@
-const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
-const aeonTest = require("./tokens/aeonTest.json");
-const bridgeUtils = require("@uniswap/token-list-bridge-utils");
+const { version } = require('../package.json');
+const mainnet = require('./tokens/mainnet.json');
+const ropsten = require('./tokens/ropsten.json');
+const rinkeby = require('./tokens/rinkeby.json');
+const goerli = require('./tokens/goerli.json');
+const kovan = require('./tokens/kovan.json');
 
 module.exports = function buildList() {
-  const parsed = version.split(".");
-  const l1List = {
-    name: "Aeon Labs Default",
-    timestamp: new Date().toISOString(),
-    version: {
-      major: +parsed[0],
-      minor: +parsed[1],
-      patch: +parsed[2],
+  const parsed = version.split('.');
+  return {
+    'name': 'Uniswap Default List',
+    'timestamp': (new Date().toISOString()),
+    'version': {
+      'major': +parsed[ 0 ],
+      'minor': +parsed[ 1 ],
+      'patch': +parsed[ 2 ]
     },
-    tags: {},
-    logoURI:
-      "https://dev-bridge.snailsteam.com/static/media/AElogo.a165e889.svg",
-    keywords: ["aeon", "default"],
+    'tags': {},
+    'logoURI': 'ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir',
+    'keywords': [
+      'uniswap',
+      'default'
+    ],
     tokens: [
       ...mainnet,
       ...ropsten,
       ...goerli,
       ...kovan,
-      ...rinkeby,
-      ...polygon,
-      ...mumbai,
-      ...aeonTest,
+      ...rinkeby
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
@@ -39,7 +34,6 @@ module.exports = function buildList() {
           return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
-      }),
+      })
   };
-  return bridgeUtils.chainify(l1List);
 };
